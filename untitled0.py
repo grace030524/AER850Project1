c4856fbf961fdaa90125af50e7a2929746b8750b
grace030524 <kawuigrace.chow@torontomu.ca>
2024-10-14 17:02:32 -0400

    Add files via upload

--- untitled0.py
+++ untitled0.py
@@ -1,6 +1,46 @@
-# dsdadddasds
+import os
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Step 1
 data = pd.read_csv('Project_1_Data.csv')
 df = pd.DataFrame(data)
 
-#12345
+# Step 2
+X = df["X"]
+Y = df["Y"]
+Z = df["Z"]
+Step = df["Step"]
+summary_stats = data.describe()
+print(summary_stats)
+
+plt.plot(Step, X, label='X')
+plt.plot(Step, Y, label='Y')
+plt.plot(Step, Z, label='Z')
+plt.title('Coordinates vs. Steps')
+plt.ylabel('Coordinates')
+plt.xlabel('Steps')
+plt.legend()
+
+plt.show()
+plt.scatter(Step, X, label='X')
+plt.scatter(Step, Y, label='Y')
+plt.scatter(Step, Z, label='Z')
+plt.title('Coordinates vs. Steps')
+plt.ylabel('Coordinates')
+plt.xlabel('Steps')
+plt.legend()
+plt.show()
+
+sns.displot(X)
+plt.show()
+sns.displot(Y)
+plt.show()
+sns.displot(Z)
+plt.show()
+
+# Step 3
+pearsoncorr = df.corr(method='pearson')
+sns.heatmap(pearsoncorr, vmin=-1, vmax=1, annot=True)
